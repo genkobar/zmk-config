4ce4dd30a145280d43cfa91ac85c707b82573eb0
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2021-12-09 17:37:07 +0000

    Make build script default to pristine builds

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 CONFIG_PATH="/workspaces/zmk-config/config"
 
-west build -d build/right -b nice_nano -- -DSHIELD=auryl_right -DZMK_CONFIG="${CONFIG_PATH}"
-west build -d build/left -b nice_nano -- -DSHIELD=auryl_left -DZMK_CONFIG="${CONFIG_PATH}"
+west build --pristine -d build/right -b nice_nano -- -DSHIELD=auryl_right -DZMK_CONFIG="${CONFIG_PATH}"
+west build --pristine -d build/left -b nice_nano -- -DSHIELD=auryl_left -DZMK_CONFIG="${CONFIG_PATH}"
