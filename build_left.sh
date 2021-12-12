c7e74c37c3082c9b42fc3dd53b313bbf52b30aed
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2021-12-12 15:25:59 +0000

    Make build scripts that copy and rename uf2

---
+++ build_left.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+CONFIG_PATH="/workspaces/zmk-config/config"
+
+west build --pristine -d build/left -b nice_nano -- -DSHIELD=auryl_left -DZMK_CONFIG="${CONFIG_PATH}"
+
+mkdir -p build/auryl
+cp build/left/zephyr/zmk.uf2 build/auryl/auryl_left.uf2 
