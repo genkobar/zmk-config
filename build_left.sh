aaa8456e43a31754383878e65062a4a42f689b6d
Valdimar Björn Ásgeirsson <vrioniavas@gmail.com>
2022-01-17 21:45:00 +0000

    tree unparsable still

--- build_left.sh
+++ build_left.sh
@@ -2,7 +2,7 @@
 
 CONFIG_PATH="/workspaces/zmk-config/config"
 
-west build --pristine -d build/left -b nice_nano -- -DSHIELD=auryl_left -DZMK_CONFIG="${CONFIG_PATH}"
+west build --pristine -d build/left -b nice_nano_v2 -- -DSHIELD=auryl_left -DZMK_CONFIG="${CONFIG_PATH}"
 
 mkdir -p build/auryl
 cp build/left/zephyr/zmk.uf2 build/auryl/auryl_left.uf2 
--- build_right.sh
+++ build_right.sh
@@ -2,7 +2,7 @@
 
 CONFIG_PATH="/workspaces/zmk-config/config"
 
-west build --pristine -d build/right -b nice_nano -- -DSHIELD=auryl_right -DZMK_CONFIG="${CONFIG_PATH}"
+west build --pristine -d build/right -b nice_nano_v2 -- -DSHIELD=auryl_right -DZMK_CONFIG="${CONFIG_PATH}"
 
 mkdir -p build/auryl
 cp build/right/zephyr/zmk.uf2 build/auryl/auryl_right.uf2 
